74440463e7851c8bba42b9a4c6f0bec3fe6b6060
smjxpro <smjxpro@yahoo.com>
2019-04-01 18:21:15 +0600

    Variables

---
+++ src/Variables.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var name = "SMJ"
+    var isAwesome = true
+
+    //var mutable, val immutable
+
+    println("Is " + name + " awesome? The answer is :" + isAwesome)
+
+    var a = 3
+    var b = 6
+    println(a + b)
+
+    val doubleNum = 123.45
+
+    val aDouble: Double = a.toDouble()
+
+    var hero: String
+
+    hero = "Batman"
+
+    println(hero)
+}
